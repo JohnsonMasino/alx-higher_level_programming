9cbb1424f9514ca3f23fe7b1b9a09f9fa4941eb0
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-01 11:56:22 -0700

    Lazy matrix multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# This funtion multiplies 2 matrices by using the module 'NumPy'
+
+import numpy as nmp
+
+def lazy_matrix_mul(m_a, m_b):
+    return (nmp.matmul(m_a, m_b))
+
+print("\nCode developed by Masino")
