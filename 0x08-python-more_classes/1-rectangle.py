863c4cfdf9bbb011bbf50d51e572d22c2a003960
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-04 02:29:16 -0700

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-#Defines
-#a class
-#rectangle
+
+#Defines a class Rectangle
+
+
 
 class Rectangle:
-    #Represents a rectangle
-    def __init__(self, width = 0, height = 0):
-        #Initialising the rectangle
+    #Representation of a rectangle
+    def __init__(self, width=0, height=0):
+        #Initializes the rectangle
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        #gets the private instance attribute, width.
-        return self.width
+        #getter for the private instance attribute width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        #sets the private instance attribute, width.
+        #setter for the private instance attribute width
         if type(value) is not int:
-            raise TypeError("Width must be an Integer")
+            raise TypeError("Width must be an integer")
         if value < 0:
             raise ValueError("Width must be greater or equal to 0")
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
-        #gets the private instatnce attribute, height
+        #getter for the private instance attribute height
         return self.__height
 
     @height.setter
     def height(self, value):
-        #Sets the private instance attribute, height
+        #setter for the private instance attribute height
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("Height must be an integer")
         if value < 0:
-            raise ValueError("Height must be greater or equl to 0")
+            raise ValueError("Height must be greater or equal to 0")
         self.__height = value
