15966a194035c96a30d796bc5bd76094a0e5374d
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-05 09:22:15 -0700

    Eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Defines a class 'Rectangle'
+"""
+
+
+class Rectangle:
+    """Represents the rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """gets the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returning the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returning the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """returning the printable string representation of the rectangle"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
+
+    def __repr__(self):
+        """returning the string representation of the rectangle for reproduction"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
