b9365bdab15291d27ce523a2d03b3f95365aa3b6
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-05 17:24:24 -0700

    Bug Fixed

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-nqueens backtracking program prints the coordinates of n queens
+nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
 
