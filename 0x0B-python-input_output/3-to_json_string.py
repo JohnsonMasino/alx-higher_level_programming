92d8be972984e6e8baeb3b4eed3b0236d18aedf3
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-12 13:44:37 -0700

    Fourth File

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This Function Contains JSON String
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
