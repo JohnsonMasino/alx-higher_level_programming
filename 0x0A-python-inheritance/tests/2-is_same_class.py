aa04f478719279040a2bc8bffe97c47f014de4f8
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-13 06:58:04 -0700

    File #

--- 0x0A-python-inheritance/tests/2-is_same_class.py
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -5,5 +5,5 @@ Contains The Function "is_same_class"
 
 
 def is_same_class(obj, a_class):
-    """return true if obj is the exact class of a_class, else, return false"""
+    """return true if obj is the exact class a_class, otherwise false"""
     return (type(obj) == a_class)
