a8d2bae9207924fe42f56cdf86eac10d8e428aa8
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-12 19:46:39 -0700

    Advanced - 1

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-Contains 'append_after' Function
+Contains the 'append after' function
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends 'new_string' after a line containing 'search_string' in 'filename'
-    """
-    lines = []
-    with open(filename, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        i = 0
-        while i < len(lines):
-            if search_string in lines[i]:
-                lines[i:i + 1] = [lines[i], new_string]
-                i += 1
-            i += 1
-    with open(filename, "w", encoding="utf-8") as f:
-        f.writelines(lines)
+    """appends 'new_string' after a line containing
+    'search_string' in 'filename' """
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
