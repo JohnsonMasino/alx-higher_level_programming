8b822f8180f5120341d49b7361dc2c75c322555a
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-01 05:08:51 -0700

    Display In Bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -I "$1" | grep "Content-Length" | awk '{print $2}'
+curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
