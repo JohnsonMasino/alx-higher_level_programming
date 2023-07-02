6afddc184058716cf771a96653a5f4a17b46222a
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-02 14:02:57 -0700

    File 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response"""
+Script that takes in a URL, sends a request to the URL and displays
+ the value of the X-Request-Id variable found in the header of the response
+ """
 import urllib.request
 import urllib.parse
 from sys import argv
