c932c2cd56248aa4aec2a525024f12e0d4b160b5
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-02 13:54:50 -0700

    File 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""urllib usage"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
