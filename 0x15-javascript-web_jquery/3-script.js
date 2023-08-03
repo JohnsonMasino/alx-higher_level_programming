b4ae23f37480d75c89ed3bdf76984886aac3c59b
JohnsonMasino <johnsonmasino@gmail.com>
2023-08-03 16:31:15 -0700

    Updated

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,17 +1,7 @@
 'use strict';
 $(() => {
-  $('DIV#toggle_header').click(() => {
-    if ($('header').hasClass('red')) {
-      $('header').removeClass('red');
-      if (!$('header').hasClass('green')) {
-        $('header').addClass('green');
-      }
-    } else if ($('header').hasClass('green')) {
-      $('header').removeClass('green');
-      if (!$('header').hasClass('red')) {
-        $('header').addClass('red');
-      }
-    } else {
+  $('DIV#red_header').click(() => {
+    if (!$('header').hasClass('red')) {
       $('header').addClass('red');
     }
   });
